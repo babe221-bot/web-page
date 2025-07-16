b618c02fee13e0093e7240bf76d8fc675b8d0d76
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-16 17:58:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,12 +1,10 @@
 import Footer from "@/components/sections/footer";
-import dynamic from 'next/dynamic'
-
-const Header = dynamic(() => import('@/components/sections/header'), { ssr: false })
-const Hero = dynamic(() => import('@/components/sections/hero'), { ssr: false })
-const Services = dynamic(() => import('@/components/sections/services'))
-const Industries = dynamic(() => import('@/components/sections/industries'))
-const Methodology = dynamic(() => import('@/components/sections/methodology'))
-const ContactSection = dynamic(() => import('@/components/sections/contact-section'))
+import Header from "@/components/sections/header";
+import Hero from "@/components/sections/hero";
+import Services from "@/components/sections/services";
+import Industries from "@/components/sections/industries";
+import Methodology from "@/components/sections/methodology";
+import ContactSection from "@/components/sections/contact-section";
 
 export default async function Home({ params: { lng } }: { params: { lng: string } }) {
   return (
