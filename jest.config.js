51dcc096942275cc2b64c273b7c441e091123102
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-16 23:52:19 +0000

    commit

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
+// jest.config.js
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
+    '\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
     '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
   },
   transform: {
-    '^.+\.(ts|tsx)$': 'ts-jest',
+    '^.+\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\.module\.(css|sass|scss)$',
+  ],
 };
