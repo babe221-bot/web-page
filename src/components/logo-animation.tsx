ef303c3ce9430c1b7cf0b1f9bf806a5bd8f669f6
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-16 16:28:38 +0000

    commit

--- src/components/logo-animation.tsx
+++ src/components/logo-animation.tsx
@@ -41,6 +41,7 @@ const LogoAnimation = () => {
           width={150}
           height={150}
           className="filter brightness-0 invert"
+          style={{ height: "auto" }}
         />
         <h1 className="text-4xl font-bold font-headline gradient-text text-center">DaorsForge AI</h1>
       </div>
