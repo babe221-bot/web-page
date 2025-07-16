b2c88bc0e3c4ed70d857caac606c272a29a62c3a
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-16 20:13:14 +0000

    commit

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,6 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
-};
+import * as serviceAccount from "../../../serviceAccountKey.json";
 
 if (getApps().length === 0) {
   initializeApp({
