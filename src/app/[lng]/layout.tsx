4fdf576740488ad74af8c7ab96aa66dc091f7957
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-17 03:06:09 +0000

    commit

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -8,7 +8,7 @@ import { Space_Grotesk, Inter, Source_Code_Pro } from 'next/font/google';
 import Script from 'next/script';
 import { GA_TRACKING_ID } from '@/lib/gtag';
 import { dir } from 'i18next'
-import { locales } from '../../i18n/settings'
+import { locales } from '@/app/i18n/settings'
 import { Providers } from '@/components/providers';
 import JazzRadioPlayer from '@/components/jazz-radio-player';
 import React from 'react';
