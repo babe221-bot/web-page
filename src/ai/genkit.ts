e3417650a8fdcacb318e2ad4c9f7a532b9928169
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-27 23:23:22 +0000

    commit

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  plugins: [googleAI({apiKey: "AIzaSyDmlzykGqwLrYR9h7cqYHLSPJaMKOfoiLc"})],
+  model: 'googleai/gemini-1.0-pro',
 });
