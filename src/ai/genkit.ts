dea0b63097210eb5eb378073b505f0a51f87df3b
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-30 00:07:29 +0000

    commit

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,7 +5,7 @@ export const ai = genkit({
   plugins: [
     googleAI({
       // Get your key from https://makersuite.google.com/app/apikey
-      apiKey: process.env.GOOGLE_API_KEY,
+      apiKey: process.env.GOOGLE_GENAI_API_KEY,
     }),
   ],
   model: 'googleai/gemini-1.5-flash-latest',
