ad24636e264efbe63d380cb08f39811fd03539a2
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-31 21:10:06 +0000

    com

--- next.config.js
+++ next.config.js
@@ -36,7 +36,8 @@ const nextConfig = {
   allowedDevOrigins: [
     "https://*.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
     "https://9004-firebase-studio-1752183138309.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
-    "https://6000-firebase-studio-1752183138309.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev"
+    "https://6000-firebase-studio-1752183138309.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
+    "https://9003-firebase-studio-1752183138309.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev"
   ],
 };
 
